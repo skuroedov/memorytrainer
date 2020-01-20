62517d3e9e81b3c2d159f434bd8fe1bf0e700e7a
Sergey Kuroedov <sergej.kuroedov@student.uzlabina.cz>
2020-01-20 14:34:26 +0100

    Fixed empty fields and bad checking

--- exercise.php
+++ exercise.php
@@ -24,11 +24,11 @@
 				$arr = explode(' ', $input);
 
 				$difficulty = 5;
-				for($i = 0; $i < sizeof($arr); $i += $difficulty) {
+				for($i = 0; $i < sizeof($arr) - 1; $i += $difficulty) {
 					do {
-						$random = rand(1, $difficulty);
+						$random = rand(0, $difficulty);
 						$pos = $i + $random;
-					} while($pos < sizeof($arr) && preg_match('/(–)/', $arr[$pos]));
+					} while($pos >= sizeof($arr) || preg_match('/(–)/', $arr[$pos]));
 					$arr[$pos] = preg_replace('/([a-žA-Ž]+)/', "<input type='text' id='s{$pos}' name='s{$pos}' autocomplete='off'>", $arr[$pos]);
 				}
 				echo "<input type='hidden' name='input' value='{$input}'>";
